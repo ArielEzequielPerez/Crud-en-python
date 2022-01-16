89262a2dd62b4f85667183d60a47f959d7ea69de
Ariel-Perez <64883289+ArielEzequielPerez@users.noreply.github.com>
2022-01-16 10:35:30 -0300

    agregue un boton para agregar libros

--- sistema/libreria/views.py
+++ sistema/libreria/views.py
@@ -4,7 +4,12 @@ from django.http import HttpResponse
 
 def inicio(request):
     return render(request, 'paginas/inicio.html')
+
 def nosotros(request):
     return render(request, 'paginas/nosotros.html')
+    
 def libros(request):
     return render(request, 'libros/index.html')
+
+def crearLibros(request):
+   return render(request, 'libros/crear.html')
